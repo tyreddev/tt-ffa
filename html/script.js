99194a0d00182d263adeebb8fcc0921b4bf281bc
tyred <tyredgeneral@gmail.com>
2026-02-18 16:28:47 +0300

    Delete html directory
    deletesa

--- html/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('message', function (event) {
-    var item = event.data;
-    if (item.action === "show") {
-        document.getElementById("container").style.display = "flex";
-    } else if (item.action === "hide") {
-        document.getElementById("container").style.display = "none";
-    }
-});
